558e2d9c1c0b399f4ceffad16e2cf00814d7ac66
Clarence <chengruichen3@gmail.com>
2024-11-03 13:46:36 +0800

    feat: import naive-ui and replace navbar
    TODO: auto import components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import Footer from '@/components/Footer.vue'
-</script>
-
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
+  <n-config-provider :theme :locale="zhCN" :date-locale="dateZhCN" :theme-overrides="themeOverrides">
+    <nav>
+      <n-flex justify="center">
+        <n-menu style="width: auto;" mode="horizontal" responsive :options="menuOptions" />
+      </n-flex>
+    </nav>
+  </n-config-provider>
   <div class="view">
     <RouterView />
   </div>
   <Footer></Footer>
 </template>
+<script setup lang="ts">
+// TODO: use auto-import, this is annoying
+
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from '@/components/Footer.vue'
+import { NConfigProvider, NGlobalStyle, GlobalThemeOverrides, darkTheme, useOsTheme, zhCN, dateZhCN } from 'naive-ui'
+import type { Component } from 'vue'
+import { h, computed } from 'vue'
+import { NMenu, NFlex } from 'naive-ui'
+import type { MenuOption } from 'naive-ui'
+
+let osThemeRef = useOsTheme()
+let theme = computed(() => (osThemeRef.value === 'dark' ? darkTheme : null))
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: "#61A2CCFF",
+    primaryColorHover: "#99DDE8FF",
+    primaryColorPressed: "#3F5D7AFF",
+    primaryColorSuppl: "#6D83AEFF"
+  }
+}
+
+const menuOptions: MenuOption[] = [
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: 'home',
+          }
+        },
+        { default: () => 'Home' }
+      ),
+    key: 'go-back-home',
+  },
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: 'about',
+          }
+        },
+        { default: () => 'About' }
+      ),
+    key: 'go-about',
+  }
+]
+</script>
 
 <style scoped>
 .logo {
