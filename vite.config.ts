a7f95e8cefec830ba5dd591eaed6989d862f3f8d
Clarence <chengruichen3@gmail.com>
2024-11-16 23:11:28 +0800

    feat: add unplugin-auto-import and unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,21 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import svgLoader from 'vite-svg-loader'
 
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: true,
+    }),
+    Components({
+      dts: true,
+      resolvers: [NaiveUiResolver()],
+    }),
     vue(),
     vueJsx(),
     svgLoader(),
